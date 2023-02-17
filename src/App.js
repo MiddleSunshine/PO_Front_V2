2adbb6863bd5b12bd3f9b15b6b2c5dcf7ef0df73
yangqingxian <qingxianyang@gmail.com>
2023-02-17 01:04:19 -0800

    路由基本配置

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import WhiteBord from "./pages/WhiteBord.jsx";
+import Index from "./pages/index.jsx";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={"/"}
+          element={<Index />}
+        />
+        <Route
+          path={"/whitebord/:id"}
+          element={<WhiteBord />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/WhiteBord.jsx
@@ -0,0 +1,10 @@
+
+const WhiteBord = () => {
+    return (
+        <div>
+            <h1>WhiteBord</h1>
+        </div>
+    )
+}
+
+export default WhiteBord
