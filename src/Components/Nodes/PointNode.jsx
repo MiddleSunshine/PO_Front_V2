45f0fe6523a4def76d5fd4fa51b67bcb39c807d0
qingxianyang <qingxianyang@gmail.com>
2023-02-20 21:54:56 +0800

    基础框架

---
+++ src/Components/Nodes/PointNode.jsx
@@ -0,0 +1,19 @@
+
+const PointNodeView=(node)=>{
+    return (
+        <h1>
+            PointNodeView
+        </h1>
+    )
+}
+
+const PointNodeEditor=(node)=>{
+    return (
+        <div></div>
+    )
+}
+
+export {
+    PointNodeView,
+    PointNodeEditor
+}
--- src/pages/WhiteBord.jsx
+++ src/pages/WhiteBord.jsx
@@ -1,10 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import ReactFlow, { useNodesState, useEdgesState } from 'reactflow';
+import 'reactflow/dist/style.css';
+import {PointNodeView} from '../Components/Nodes/PointNode'
 
-const WhiteBord = () => {
-    return (
-        <div>
-            <h1>WhiteBord</h1>
-        </div>
-    )
+const initialNodes = [
+    { id: '1',type:"PointNodeView", data: { label: '-' }, position: { x: 100, y: 100 } },
+    { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 200 } },
+];
+
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const defaultViewport = { x: 0, y: 0, zoom: 1.5 };
+
+const AllNodeTypes={
+    PointNodeView
 }
 
-export default WhiteBord
+const WhiteBord = () => {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+    return (
+        <ReactFlow
+            nodeTypes={AllNodeTypes}
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            defaultViewport={defaultViewport}
+            minZoom={0.2}
+            maxZoom={4}
+            attributionPosition="bottom-left"
+        >
+        </ReactFlow>
+    );
+};
+
+export default WhiteBord;
