ef98b3e897309f60099395b68cab9c747d635d71
qingxianyang <qingxianyang@gmail.com>
2023-02-20 22:31:31 +0800

    部分代码优化

--- src/pages/WhiteBord.jsx
+++ src/pages/WhiteBord.jsx
@@ -18,6 +18,48 @@ const AllNodeTypes={
 const BasicBord=()=>{
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+    const [showMenu,setShowMenu]=useState(false);
+    const [menuPosition,setMenuPosition]=useState({x:0,y:0});
+
+    const renderMenu=()=>{
+        const menuStyle={
+            position: 'absolute',
+            left: menuPosition.x,
+            top: menuPosition.y,
+            backgroundColor: 'white',
+            padding: '10px',
+            borderRadius: '5px',
+            boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)',
+        }
+
+        return (
+            <div style={menuStyle}>
+                <ul>
+                    <li>菜单1</li>
+                    <li>菜单2</li>
+                    <li>菜单3</li>
+                </ul>
+            </div>
+        )
+    }
+    /**
+     *
+     * @param MouseEvent event
+     */
+    const handleContextMenu=(event)=>{
+        event.preventDefault();
+        debugger
+        // new Promise(resolve => {}).then(()=>{
+        //     setMenuPosition({
+        //         x:event.clientX,
+        //         y:event.clientY
+        //     })
+        // }).then(()=>{
+        //     setShowMenu(true);
+        // })
+    }
+
     return (
         <ReactFlow
             nodeTypes={AllNodeTypes}
@@ -29,7 +71,17 @@ const BasicBord=()=>{
             minZoom={0.2}
             maxZoom={4}
             attributionPosition="bottom-left"
+            onNodeContextMenu={(event,node)=>{
+                event.preventDefault();
+                // 这里会在 node 被右键点击的时候触发
+            }}
+            // onContextMenu={handleContextMenu}
         >
+            {/*{*/}
+            {/*    showMenu*/}
+            {/*        ?renderMenu()*/}
+            {/*        :''*/}
+            {/*}*/}
         </ReactFlow>
     )
 }
