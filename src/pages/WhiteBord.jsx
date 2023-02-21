0d7bc993cca897db0e5253b0ceb293d771aad081
yangqingxian <qingxianyang@gmail.com>
2023-02-20 19:30:58 -0800

    right click to call menu

--- src/pages/WhiteBord.jsx
+++ src/pages/WhiteBord.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactFlow, {useNodesState, useEdgesState, ReactFlowProvider} from 'reactflow';
 import 'reactflow/dist/style.css';
 import {PointNodeView} from '../Components/Nodes/PointNode'
 
 const initialNodes = [
-    { id: '1',type:"PointNodeView", data: { label: '-' }, position: { x: 100, y: 100 } },
-    { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 200 } },
+    {id: '1', type: "PointNodeView", data: {label: '-'}, position: {x: 100, y: 100}},
+    {id: '2', data: {label: 'Node 2'}, position: {x: 100, y: 200}},
 ];
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
-const defaultViewport = { x: 0, y: 0, zoom: 1.5 };
+const initialEdges = [{id: 'e1-2', source: '1', target: '2'}];
+const defaultViewport = {x: 0, y: 0, zoom: 1.5};
 
-const AllNodeTypes={
+const AllNodeTypes = {
     PointNodeView
 }
 
-const BasicBord=()=>{
+const BasicBord = () => {
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
-    const [showMenu,setShowMenu]=useState(false);
-    const [menuPosition,setMenuPosition]=useState({x:0,y:0});
+    const [showMenu, setShowMenu] = useState(false);
+    const [menuPosition, setMenuPosition] = useState({x: 0, y: 0});
 
-    const renderMenu=()=>{
-        const menuStyle={
+    const renderMenu = () => {
+        const menuStyle = {
             position: 'absolute',
             left: menuPosition.x,
             top: menuPosition.y,
@@ -47,17 +47,12 @@ const BasicBord=()=>{
      *
      * @param MouseEvent event
      */
-    const handleContextMenu=(event)=>{
+    const handleContextMenu = (event) => {
         event.preventDefault();
-        debugger
-        // new Promise(resolve => {}).then(()=>{
-        //     setMenuPosition({
-        //         x:event.clientX,
-        //         y:event.clientY
-        //     })
-        // }).then(()=>{
-        //     setShowMenu(true);
-        // })
+        setMenuPosition({
+            x: event.clientX,
+            y: event.clientY
+        })
     }
 
     return (
@@ -71,17 +66,17 @@ const BasicBord=()=>{
             minZoom={0.2}
             maxZoom={4}
             attributionPosition="bottom-left"
-            onNodeContextMenu={(event,node)=>{
+            onNodeContextMenu={(event, node) => {
                 event.preventDefault();
                 // 这里会在 node 被右键点击的时候触发
             }}
             onPaneContextMenu={handleContextMenu}
         >
-            {/*{*/}
-            {/*    showMenu*/}
-            {/*        ?renderMenu()*/}
-            {/*        :''*/}
-            {/*}*/}
+            {
+                menuPosition.x > 0 && menuPosition.y > 0
+                    ? renderMenu()
+                    : ''
+            }
         </ReactFlow>
     )
 }
@@ -89,7 +84,7 @@ const BasicBord=()=>{
 const WhiteBord = () => {
     return (
         <ReactFlowProvider>
-            <BasicBord />
+            <BasicBord/>
         </ReactFlowProvider>
     );
 };
