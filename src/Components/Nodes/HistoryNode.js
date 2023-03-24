168c27250d74940d7dcf5e9ba9301201578b8100
YangQingXian <qingxianyang@gmail.com>
2023-03-24 21:39:55 +0800

    历史代码

--- src/Components/Nodes/HistoryNode.js
+++ src/Components/Nodes/HistoryNode.js
@@ -1,7 +1,35 @@
+import {Input, message} from "antd";
+import {useState} from "react";
+import {requestAPI} from "../../config/function";
 
 const HistoryNode=()=>{
-    return <div>
+    const [keyword,setkeyword]=useState('');
 
+    const searchNode=(searchKeyword)=>{
+        if (!searchKeyword){
+            message.warning("Please input the keyword");
+            return false;
+        }
+        requestAPI('index.php?action=NodeController&method=SearchNode',{
+            method:"post",
+            body:JSON.stringify({
+                keyword:searchKeyword
+            })
+        })
+            .then((res)=>{
+                res.json().then((json)=>{
+                    // todo 这里开始接着写
+                })
+            })
+    }
+
+    return <div>
+        <Input
+            value={keyword}
+            onChange={(e)=>{
+                setkeyword(e.target.value);
+            }}
+        />
     </div>
 }
 
