eaa063b7e37c3f8f3c234eea568c41ece3e809af
YangQingXian <qingxianyang@gmail.com>
2023-03-29 23:00:51 +0800

    保存部分效果

--- src/Components/Nodes/DirectoryNode.js
+++ src/Components/Nodes/DirectoryNode.js
@@ -1,87 +1,159 @@
 import {useState} from "react";
-import {Button, Col, Input, Row} from "antd";
+import React from "react";
+import {Button, Col, Divider, Input, List, Row, Select, Timeline} from "antd";
+import {
+    FolderOutlined,
+    FileOutlined,
+    CaretUpOutlined,
+    CaretDownOutlined,
+    CaretLeftOutlined,
+    CaretRightOutlined,
+    CloseCircleOutlined
+} from '@ant-design/icons'
+import {NodeResizer} from "@reactflow/node-resizer";
 
+const ICON_LENGTH=2;
+const LENGTH_AMOUNT=24;
+const TYPE_FILE='File';
+const TYPE_FOLDER='Folder';
 
-const DirectoryNode=(nodeProps)=>{
-    const [nodeData,setNodeData]=useState(nodeProps.node_data);
+const DirectoryNode=React.memo((nodeProps)=>{
+
+    const [nodeData,setNodeData]=useState([
+        {
+            node_data:{
+                type:"Folder",
+                offset:0
+            },
+            data:{
+                Title:"2"
+            }
+        },
+        {
+            node_data:{
+                type:"File",
+                offset:2
+            },
+            data:{
+                Title:"3"
+            }
+        },
+        {
+            node_data:{
+                type:"File",
+                offset:4
+            },
+            data:{
+                Title:"3"
+            }
+        },
+        {
+            node_data:{
+                type:"Folder",
+                offset:0
+            },
+            data:{
+                Title:"2"
+            }
+        },
+        {
+            node_data:{
+                type:"File",
+                offset:2
+            },
+            data:{
+                Title:"3"
+            }
+        },
+    ]);
+
     return (
-        <div>
-            {
-                nodeData?.nodes.map((n)=>{
+        <div className={"DirectoryNode"}>
+            <NodeResizer
+                isVisible={nodeProps.selected}
+                color="#ff0071"
+            />
+            <Row>
+                <Input
+
+                />
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <div>
+                        <Button
+                            size={"small"}
+                            type={"link"}
+                            icon={<CaretUpOutlined />}
+                        ></Button>
+                        <Button
+                            size={"small"}
+                            type={"link"}
+                            icon={<CaretDownOutlined />}
+                        ></Button>
+                        <Button
+                            size={"small"}
+                            type={"link"}
+                            icon={<CaretLeftOutlined />}
+                        ></Button>
+                        <Button
+                            size={"small"}
+                            type={"link"}
+                            icon={<CaretRightOutlined />}
+                        ></Button>
+                    </div>
+                </Col>
+            </Row>
+            <List
+                dataSource={nodeData}
+                renderItem={(n)=>{
                     return (
-                        <Row>
+                        <Row
+                            className={"EachRow"}
+                            justify={"start"}
+                            align={"middle"}
+                        >
                             <Col
                                 offset={n.node_data.offset}
-                                span={1}
+                                span={ICON_LENGTH}
                             >
-
+                                {
+                                    n.node_data.type==TYPE_FOLDER
+                                        ?<FolderOutlined />
+                                        :<FileOutlined />
+                                }
                             </Col>
-                            <Col span={23-n.node_data.offset}>
+                            <Col
+                                span={LENGTH_AMOUNT-ICON_LENGTH-n.node_data.offset}
+                            >
                                 <Input
                                     value={n.data.Title}
-                                    addonAfter={()=>{
-                                        return (
-                                            <div>
-                                                <Button
-                                                    size={"small"}
-                                                    type={"link"}
-                                                >
-                                                </Button>
-                                                <Button
-                                                    type={"link"}
-                                                    size={"small"}
-                                                >
-
-                                                </Button>
-                                            </div>
-                                        )
-                                    }}
-                                    addonBefore={()=>{
-                                        return (
-                                            <div>
-                                                <Button
-                                                    type={"link"}
-                                                    size={"small"}
-                                                >
-                                                </Button>
-                                                <Button
-                                                    type={"link"}
-                                                    size={"small"}
-                                                >
-                                                </Button>
-                                            </div>
-                                        )
-                                    }}
+                                    addonAfter={
+                                        <Button
+                                            size={"small"}
+                                            type={"link"}
+                                            icon={<CloseCircleOutlined />}
+                                        >
+                                        </Button>
+                                    }
                                 />
                             </Col>
                         </Row>
                     )
-                })
-            }
+                }}
+            />
+            <Row>
+                <Col span={ICON_LENGTH}>
+                    
+                </Col>
+                <Col span={24-ICON_LENGTH}>
+                    <Input />
+                </Col>
+            </Row>
         </div>
     )
-}
+})
 
 export {
     DirectoryNode
 }
-
-/**
- * nodeProps={
- *     node_data:{
- *         nodes:[
- *             {
- *                 node_data:{
- *                     offset:""
- *                 },
- *                 data:{
- *                     Title:""
- *                 }
- *             }
- *         ]
- *     },
- *     data:{}
- * }
- *
- */
-
