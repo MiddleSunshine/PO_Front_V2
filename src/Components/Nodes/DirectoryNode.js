a4d563e7a1451dc388cafa11e3163226914c5b2f
yangqingxian <qingxianyang@gmail.com>
2023-03-29 03:26:38 -0700

    保存部分代码

--- src/Components/Nodes/DirectoryNode.js
+++ src/Components/Nodes/DirectoryNode.js
@@ -1,11 +1,87 @@
+import {useState} from "react";
+import {Button, Col, Input, Row} from "antd";
 
-const DirectoryNode=()=>{
+
+const DirectoryNode=(nodeProps)=>{
+    const [nodeData,setNodeData]=useState(nodeProps.node_data);
     return (
-        <div></div>
+        <div>
+            {
+                nodeData?.nodes.map((n)=>{
+                    return (
+                        <Row>
+                            <Col
+                                offset={n.node_data.offset}
+                                span={1}
+                            >
+
+                            </Col>
+                            <Col span={23-n.node_data.offset}>
+                                <Input
+                                    value={n.data.Title}
+                                    addonAfter={()=>{
+                                        return (
+                                            <div>
+                                                <Button
+                                                    size={"small"}
+                                                    type={"link"}
+                                                >
+                                                </Button>
+                                                <Button
+                                                    type={"link"}
+                                                    size={"small"}
+                                                >
+
+                                                </Button>
+                                            </div>
+                                        )
+                                    }}
+                                    addonBefore={()=>{
+                                        return (
+                                            <div>
+                                                <Button
+                                                    type={"link"}
+                                                    size={"small"}
+                                                >
+                                                </Button>
+                                                <Button
+                                                    type={"link"}
+                                                    size={"small"}
+                                                >
+                                                </Button>
+                                            </div>
+                                        )
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                    )
+                })
+            }
+        </div>
     )
 }
 
 export {
     DirectoryNode
 }
+
+/**
+ * nodeProps={
+ *     node_data:{
+ *         nodes:[
+ *             {
+ *                 node_data:{
+ *                     offset:""
+ *                 },
+ *                 data:{
+ *                     Title:""
+ *                 }
+ *             }
+ *         ]
+ *     },
+ *     data:{}
+ * }
+ *
+ */
 
