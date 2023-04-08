5ef5250af7a9e7701931d0657cbb6323380ffa05
YangQingXian <qingxianyang@gmail.com>
2023-04-08 23:16:28 +0800

    首页样式设置一下

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,73 @@
-
+import {Col, Image, Input, message, Row} from "antd";
+import Rainbow from '../Images/rainbow.png'
+import {useEffect, useState} from "react";
+import {requestAPI} from "../config/function";
 const Index = () => {
+
+    const [settings,setSettings]=useState({
+        Title:""
+    });
+
+    useEffect(()=>{
+        GetIndex();
+    },[])
+
+    const GetIndex=()=>{
+        requestAPI("index.php?action=IndexController&method=Index",false)
+            .then((res)=>{
+                if (res.Data){
+                    setSettings(res.Data);
+                }else{
+                    message.warning(res.Message);
+                }
+            })
+    }
+
     return (
-        <div>
-            <h1>Index</h1>
+        <div
+            className={"Index"}
+        >
+            <Row
+                style={{height:"inherit"}}
+                justify={"center"}
+                align={"middle"}
+            >
+                <Col span={24}>
+                    <Row
+                        justify={"center"}
+                        align={"middle"}
+                        style={{textAlign:"center"}}
+                    >
+                        <Col
+                            span={6}
+                            className={"rainbow-letters"}
+                        >
+                            <Image
+                                style={{width:"30px"}}
+                                src={Rainbow}
+                            />
+                            <span
+                                className={"Title"}
+                            >
+                                {
+                                    settings.Title
+                                }
+                            </span>
+                        </Col>
+                    </Row>
+                    <Row
+                        justify={"center"}
+                        align={"middle"}
+                    >
+                        <Col span={6}>
+                            <Input
+
+                            />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+
         </div>
     )
 }
