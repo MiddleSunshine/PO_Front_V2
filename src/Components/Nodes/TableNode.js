993a0996512c04cde34e4db97efbc529d844ec75
yangqingxian <qingxianyang@gmail.com>
2023-04-11 03:15:32 -0700

    保存部分代码

--- src/Components/Nodes/TableNode.js
+++ src/Components/Nodes/TableNode.js
@@ -14,3 +14,31 @@ const TableNode=(nodeProps)=>{
 export {
     TableNode
 }
+
+/**
+ let titleIndexMap=[
+ "column_3",
+ "column_2",
+ "column_1"
+ ];
+
+ let titles=[];
+
+ let columns=[];
+
+ let length=3;
+ let height=10;
+ for (let i=1;i<=length;i++){
+    titles[i]={
+        title:i,
+        dataIndex:titleIndexMap[i-1]
+    };
+}
+
+ for (let row=0;row<height;row++){
+    columns[row]={};
+    for (let i=1;i<=length;i++){
+        columns[row][titleIndexMap[i-1]]=1;
+    }
+}
+ */
