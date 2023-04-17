ecf413b0d9d924b96643c72dd13fb29896130cbd
yangqingxian <qingxianyang@gmail.com>
2023-04-17 03:12:17 -0700

    取消 onBlur 效果

--- src/Components/Nodes/TitleNode.js
+++ src/Components/Nodes/TitleNode.js
@@ -1,42 +1,42 @@
-import {Button, Input, message} from "antd";
-import {useCallback, useState} from "react";
-import {CreateNodeAsync, GetNodeStyle} from "./BasicNode";
-import {Handle, useReactFlow} from 'reactflow';
-import {UpdateNode} from "./BasicNode";
-import {NodeResizer} from "@reactflow/node-resizer";
-import {SaveOutlined} from "@ant-design/icons";
-const MODE_VIEW='View';
-const MODE_EDIT='Edit';
+import { Button, Input, message } from "antd";
+import { useCallback, useState } from "react";
+import { CreateNodeAsync, GetNodeStyle } from "./BasicNode";
+import { Handle, useReactFlow } from 'reactflow';
+import { UpdateNode } from "./BasicNode";
+import { NodeResizer } from "@reactflow/node-resizer";
+import { SaveOutlined } from "@ant-design/icons";
+const MODE_VIEW = 'View';
+const MODE_EDIT = 'Edit';
 
-const TitleNode=(nodeProps)=>{
-    const [mode,setMode]=useState(nodeProps.data.data.hasOwnProperty('ID')?MODE_VIEW:MODE_EDIT);
-    const [data,setData]=useState(nodeProps.data.data);
-    const instance=useReactFlow();
+const TitleNode = (nodeProps) => {
+    const [mode, setMode] = useState(nodeProps.data.data.hasOwnProperty('ID') ? MODE_VIEW : MODE_EDIT);
+    const [data, setData] = useState(nodeProps.data.data);
+    const instance = useReactFlow();
 
-    const SAVE_DATA=(data)=>{
-        if (data.hasOwnProperty('ID')){
-            let newNode=nodeProps;
-            newNode.data.data=data;
-            UpdateNode(instance,newNode);
+    const SAVE_DATA = (data) => {
+        if (data.hasOwnProperty('ID')) {
+            let newNode = nodeProps;
+            newNode.data.data = data;
+            UpdateNode(instance, newNode);
         }
     }
 
-    const finishInput=()=>{
-        if (!data?.Name){
+    const finishInput = () => {
+        if (!data?.Name) {
             message.warning("Please input the title");
             return false;
         }
-        if (!data.hasOwnProperty('ID')){
-            CreateNodeAsync('TitleNode',nodeProps.id,data.Name)
-                .then((res)=>{
-                    if (res.Data.data){
+        if (!data.hasOwnProperty('ID')) {
+            CreateNodeAsync('TitleNode', nodeProps.id, data.Name)
+                .then((res) => {
+                    if (res.Data.data) {
                         setData(res.Data.data)
                         SAVE_DATA(res.Data.data);
-                    }else{
+                    } else {
                         message.warning(res.Message);
                     }
                 })
-        }else{
+        } else {
             SAVE_DATA(data);
         }
         setMode(MODE_VIEW);
@@ -62,26 +62,23 @@ const TitleNode=(nodeProps)=>{
         />
         <div className={"Content"}>
             {
-                mode==MODE_VIEW
-                    ?<h3
-                        onClick={()=>{
+                mode == MODE_VIEW
+                    ? <h3
+                        onClick={() => {
                             setMode(MODE_EDIT);
                         }}
                     >
                         {data.Name}
                     </h3>
-                    :<Input
+                    : <Input
                         value={data.Name}
-                        onChange={(e)=>{
+                        onChange={(e) => {
                             setData({
                                 ...data,
-                                Name:e.target.value
+                                Name: e.target.value
                             })
                         }}
-                        onPressEnter={()=>{
-                            finishInput();
-                        }}
-                        onBlur={()=>{
+                        onPressEnter={() => {
                             finishInput();
                         }}
                         addonAfter={
@@ -89,7 +86,7 @@ const TitleNode=(nodeProps)=>{
                                 size={"small"}
                                 type={"link"}
                                 icon={<SaveOutlined />}
-                                onClick={()=>{
+                                onClick={() => {
                                     finishInput();
                                 }}
                             >
