e3d94a00f7d796389448e60e3b695144e3310f63
yangqingxian <qingxianyang@gmail.com>
2023-04-18 02:50:10 -0700

    保存数据

--- src/Components/Nodes/SheetNode.js
+++ src/Components/Nodes/SheetNode.js
@@ -5,21 +5,61 @@ import {
     ExportOutlined,
     SaveOutlined
 } from '@ant-design/icons';
+import { CreateNodeAsync, GetNodeStyle, UpdateNode } from "./BasicNode";
+import { Handle, useReactFlow, NodeResizer } from 'reactflow';
 // 节点
 const SheetNode = (nodeProps) => {
     const [data, setData] = useState(nodeProps.data.data);
-
+    const instance = useReactFlow();
     const finishInput = (name) => {
-
+        let newData = { ...data };
+        newData.Name = name;
+        if (!newData.hasOwnProperty('ID')) {
+            CreateNodeAsync('SheetNode', name, nodeProps.id)
+                .then((res) => {
+                    if (res.Data.data.ID) {
+                        setData(res.Data.data);
+                        newData = res.Data.data;
+                    }
+                })
+        }
+        setData(newData);
+        SAVE_INTO(newData);
     }
 
+    const SAVE_INTO = (newData) => {
+        if (newData.hasOwnProperty('ID')) {
+            let newNode = { ...nodeProps };
+            newNode.data.data = newData;
+            UpdateNode(instance, newNode);
+        }
+    }
+
+
     return (
-        <div className="SheetNode">
+        <div
+            className="SheetNode"
+            style={GetNodeStyle(nodeProps)}
+        >
+            <Handle
+                className={"TargetConnection"}
+                type={"target"}
+                position={"left"}
+            />
+            <NodeResizer
+                isVisible={nodeProps.selected}
+            />
             <Input
                 addonBefore={<FileExcelOutlined />}
                 value={data?.Name}
                 onChange={(e) => {
-                    finishInput(e.target.value);
+                    setData({
+                        ...data,
+                        Name: e.target.value
+                    });
+                }}
+                onPressEnter={() => {
+                    finishInput(data?.Name)
                 }}
                 addonAfter={
                     data?.ID
@@ -27,12 +67,16 @@ const SheetNode = (nodeProps) => {
                             size="small"
                             type="link"
                             icon={<ExportOutlined />}
-                            href="/sheet/"
+                            href={`/sheet/${data.ID}`}
+                            target="_blank"
                         ></Button>
                         : <Button
                             size="small"
                             type="link"
                             icon={<SaveOutlined />}
+                            onClick={() => {
+                                finishInput(data?.Name)
+                            }}
                         ></Button>
                 }
             />
