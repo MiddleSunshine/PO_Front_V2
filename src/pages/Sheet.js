8b2f57c34cddfede07189aa62af68d0c520ac2d5
yangqingxian <qingxianyang@gmail.com>
2023-04-18 02:59:43 -0700

    修复页面

--- src/pages/Sheet.js
+++ src/pages/Sheet.js
@@ -1,20 +1,36 @@
-
+import { useParams } from "react-router-dom";
 import { Workbook } from "@fortune-sheet/react";
 import "@fortune-sheet/react/dist/index.css"
 import { useState } from "react";
+import { GetNodeDetailAsync } from "../Components/Nodes/BasicNode";
 
 // 编辑页面
 const EditSheetNode = () => {
-    const [data, setData] = useState();
+    const { id } = useParams();
+    const [data, setData] = useState({});
+    const [nodeData, setNodeData] = useState({
+        sheet: [{ name: "Sheet1" }]
+    });
+
+    const getTableDetail = (NodeId) => {
+        GetNodeDetailAsync(NodeId)
+            .then((res) => {
+                if (res.Data.node_data) {
+
+                } else {
+
+                }
+            })
+    }
 
     return (
         <div
             className="EditSheetNode"
         >
             <Workbook
-                data={data}
+                data={nodeData.sheet}
                 onChange={(newData) => {
-                    setData(newData);
+
                 }}
             />
         </div>
