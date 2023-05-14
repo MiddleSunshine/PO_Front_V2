cb728195ecf31746292de457b138ea8c15232005
YangQingXian <qingxianyang@gmail.com>
2023-05-14 11:46:16 +0800

    页面部分效果完成

--- src/Components/CalendarComponent.js
+++ src/Components/CalendarComponent.js
@@ -5,40 +5,109 @@ import listPlugin from '@fullcalendar/list';
 import cnLocales from '@fullcalendar/core/locales/zh-cn'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import {useEffect, useState} from 'react';
-import {Button, Checkbox, Col, DatePicker, Divider, Form, Input, Modal, Row, TimePicker} from "antd";
+import {Button, Checkbox, DatePicker, Divider, Form, Input, message, Modal, TimePicker} from "antd";
 import locale from 'antd/es/date-picker/locale/zh_CN';
 import dayjs from "dayjs";
+import {CirclePicker} from '@hello-pangea/color-picker'
+import {getId} from "../config/WhiteBord";
 
 const MODE_LIST = 'listWeek';
 const MODE_MONTH = 'dayGridMonth';
 const MODE_WEEK = 'timeGridWeek';
+
 const DATE_FORMAT = 'YYYY-MM-DD';
 const TIME_FORMAT = 'HH:mm';
 
-const EVENT_TEMPLATE = {
+const EDIT_EVENT_TEMPLATE = {
+    id: "",
     title: "",
     content: "",
-    start: '',
-    end: '',
     start_date: dayjs(),
     end_date: dayjs(),
     start_time: dayjs(),
     end_time: dayjs(),
     background: "",
+    fontColor: "",
     fullDay: true
+}
+
+const EVENT_TEMPLATE = {
+    id: "",
+    title: "",
+    start: "",
+    end: "",
+    backgroundColor: "",
+    textColor: "",
+    timeText: "",
+    allDay: true
 }
 
 const CalendarComponent = () => {
     const {id} = useParams();
+    // 所有事件
     const [events, setEvents] = useState([]);
+    //
+    const [databaseEvent, setDatabaseEvent] = useState([]);
+    // 编辑部分
     const [editEvent, setEditEvent] = useState({
-        ...EVENT_TEMPLATE
+        ...EDIT_EVENT_TEMPLATE
     });
+    const [editMode, setEditMode] = useState(false);
+
+    const createEvent = () => {
+        if (!editEvent.start_date || !editEvent.end_date) {
+            message.warning("请输入日期");
+            return false;
+        }
+        if (!editEvent.fullDay) {
+            // 不是全天
+            if (!editEvent.start_time || !editEvent.end_time) {
+                message.warning("请输入具体时间");
+                return false;
+            }
+        }
+        let newEvent={
+            ...EVENT_TEMPLATE
+        }
+        let newDatabaseEvent={
+            ...editEvent,
+            id:getId('full_calendar')
+        }
+        newEvent=changeDatabaseEventIntoEvent(newEvent,newDatabaseEvent);
+        // 保存数据
+        setEvents(events => events.concat(newEvent));
+        setDatabaseEvent(databaseEvents => databaseEvents.concat(newDatabaseEvent));
+        // 结束编辑
+        setEditMode(false);
+        let historyEvent=events;
+        debugger
+    }
+
+    const changeDatabaseEventIntoEvent=(event,databaseEvent)=>{
+        event.id = databaseEvent.id;
+        event.title = databaseEvent.title;
+        let startTime, endTime,format;
+        format=`${DATE_FORMAT} ${TIME_FORMAT}`
+        if (event.fullDay) {
+            startTime = `${databaseEvent.start_date} 00:00`
+            endTime = `${databaseEvent.end_date} 23:59`
+        } else {
+            startTime = `${databaseEvent.start_date} ${databaseEvent.start_time}`
+            endTime = `${databaseEvent.end_date} ${databaseEvent.end_time}`
+        }
+        event.start = dayjs(startTime,format).toISOString();
+        event.end = dayjs(endTime,format).toISOString();
+        event.backgroundColor = databaseEvent.background;
+        event.textColor = databaseEvent.fontColor;
+        event.allDay = databaseEvent.fullDay;
+        return event;
+    }
+
     const renderEvent = (eventInfo) => {
         return (
             <div
                 onClick={() => {
-                    // todo 这里进行编辑效果
+
                 }}
             >
                 <b>{eventInfo.timeText}</b>
@@ -51,9 +120,9 @@ const CalendarComponent = () => {
         let event = {...editEvent};
         switch (key) {
             case 'time':
-                event.start_time=dayjs(value[0],TIME_FORMAT);
-                event.end_time=dayjs(value[1],TIME_FORMAT);
-                event.fullDay=false;
+                event.start_time = dayjs(value[0], TIME_FORMAT);
+                event.end_time = dayjs(value[1], TIME_FORMAT);
+                event.fullDay = false;
                 break;
             case 'date':
                 event.start_date = dayjs(value[0], DATE_FORMAT);
@@ -93,10 +162,17 @@ const CalendarComponent = () => {
                 >
                     保存修改
                 </Button>
-                &nbsp;&nbsp;
-                /
-                &nbsp;&nbsp;
-                <Button>
+                <Divider
+                    type={"vertical"}
+                />
+                <Button
+                    onClick={() => {
+                        setEditEvent({
+                            ...EDIT_EVENT_TEMPLATE
+                        });
+                        setEditMode(true)
+                    }}
+                >
                     新增事项
                 </Button>
             </Divider>
@@ -116,12 +192,24 @@ const CalendarComponent = () => {
                 dayCellContent={renderDayCellContent}
             />
             <Modal
-                open={true}
+                open={editMode}
                 width={1200}
+                onCancel={() => {
+                    setEditMode(false);
+                }}
+                onOk={() => {
+                    if (editEvent.id) {
+                        // 编辑
+                    } else {
+                        // 新增
+                        createEvent();
+                    }
+                }}
             >
                 <Form>
                     <Form.Item
                         label={"日期"}
+                        required={true}
                     >
                         <DatePicker.RangePicker
                             locale={locale}
@@ -154,12 +242,13 @@ const CalendarComponent = () => {
                                 [dayjs(editEvent.start_time, TIME_FORMAT), dayjs(editEvent.end_time, TIME_FORMAT)]
                             }
                             onChange={(timeArray) => {
-                                updateEditEvent('time',timeArray)
+                                updateEditEvent('time', timeArray)
                             }}
                         />
                     </Form.Item>
                     <Form.Item
                         label={"标题"}
+                        required={true}
                     >
                         <Input
                             value={editEvent.title}
@@ -181,7 +270,22 @@ const CalendarComponent = () => {
                     <Form.Item
                         label={"背景色"}
                     >
-
+                        <CirclePicker
+                            defaultColor={editEvent.background}
+                            onChange={(color) => {
+                                updateEditEvent('background', color.hex)
+                            }}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label={"字体色"}
+                    >
+                        <CirclePicker
+                            defaultColor={editEvent.fontColor}
+                            onChange={(color) => {
+                                updateEditEvent('fontColor', color.hex)
+                            }}
+                        />
                     </Form.Item>
                 </Form>
             </Modal>
